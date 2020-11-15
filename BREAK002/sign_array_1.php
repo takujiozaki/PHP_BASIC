e98ffd3efd066aced63fe34cbcc081bf2a62b67e
takujiozaki <takujiozaki@gmail.com>
2020-11-15 21:40:58 +0900

    コード例

---
+++ BREAK002/sign_array_1.php
@@ -0,0 +1,16 @@
+<?php
+//星座を格納する配列
+$sign_array = array(
+    ['jp'=>'牡羊座','en'=>'aries'],
+    ['jp'=>'牡牛座', 'en'=>'taurus'],
+    ['jp'=>'双子座', 'en'=>'gemini'],
+    ['jp'=>'蟹座', 'en'=>'cancer'],
+    ['jp'=>'獅子座', 'en'=>'leo'],
+    ['jp'=>'乙女座', 'en'=>'virgo'],
+    ['jp'=>'天秤座', 'en'=>'libra'],
+    ['jp'=>'蠍座', 'en'=>'scorpio'],
+    ['jp'=>'射手座', 'en'=>'sagittarius'],
+    ['jp'=>'山羊座', 'en'=>'capricorn'],
+    ['jp'=>'水瓶座', 'en'=>'aquarius'],
+    ['jp'=>'魚座', 'en'=>'pisces'],
+);
