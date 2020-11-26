e0b5daf79373d220f33d54e61137ce731ffe0d7d
Takuji Ozaki <42739202+takujiozaki@users.noreply.github.com>
2020-11-26 15:44:42 +0900

    Update FileObject.php

--- SAMPLE07/FileObject.php
+++ SAMPLE07/FileObject.php
@@ -12,6 +12,22 @@ class FileObject{
         $fp = fopen($this->filename,'r');
         $status = fread($fp,8192);
         fclose($fp);
-        return $status;
+        if($status!=""){
+            return "開店中";
+        }else{
+            return "準備中";
+        }
+    }
+
+    public function clear(){
+        $fp = fopen($this->filename,'w');
+        fwrite($fp,"");
+        fclose($fp);
+    }
+    
+    public function write(){
+        $fp = fopen($this->filename,'w');
+        fwrite($fp,"1");
+        fclose($fp);
     }
 }
