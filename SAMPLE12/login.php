003089a36b29c4940e93c00e7085b3c7e9d6a3fd
takujiozaki <takujiozaki@gmail.com>
2020-12-21 11:01:47 +0900

    ログイン画面　エラー表示

--- SAMPLE12/login.php
+++ SAMPLE12/login.php
@@ -6,6 +6,16 @@
  * login_result.phpにPOSTする
  */
 
+if($_SERVER['REQUEST_METHOD'] === "GET"){
+    @session_start();
+    if(isset($_SESSION['err_msgs'])){
+        $err_msgs = $_SESSION['err_msgs'];
+        unset($_SESSION['err_msgs']);
+    }
+}else{
+    die('このページにはPOSTでアクセス出来ません。');
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -16,6 +26,14 @@
 </head>
 <body>
 <h1>LOGIN</h1>
+
+<?php if(count($err_msgs)):?>
+<ul>
+<?php foreach($err_msgs as $err_msg):?>
+<li><?=$err_msg?></li>
+<?php endforeach ?>
+</ul>
+<?php endif ?>
 <form action="login_result.php" method="post">
 <div>
 <label for="userid">ユーザーID</label>
