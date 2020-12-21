6834f9e56fd47f9b6e02d370c9867da060b01285
takujiozaki <takujiozaki@gmail.com>
2020-12-21 10:33:08 +0900

    ログイン処理

--- SAMPLE12/login.php
+++ SAMPLE12/login.php
@@ -5,3 +5,27 @@
  * ユーザー情報を入力するプログラム
  * login_result.phpにPOSTする
  */
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>LOGIN</h1>
+<form action="login_result.php" method="post">
+<div>
+<label for="userid">ユーザーID</label>
+<input type="text" name="userid" id="userid">
+</div>
+<div>
+<label for="password">パスワード</label>
+<input type="password" name="password" id="password">
+</div>
+<button type="submit">ログイン</button>
+</form> 
+</body>
+</html>
--- SAMPLE12/login_result.php
+++ SAMPLE12/login_result.php
@@ -5,3 +5,17 @@
  * ログイン処理をするプログラム
  * login.phpにPOSTされた情報を元にログイン処理を行い結果を表示する
  */
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>LOGIN</h1>
+<p>ログインが成功（失敗）しました。</p>
+</body>
+</html>
